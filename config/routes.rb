9e7ab7557d968f8a41f0fec91a1a5650c1ee6e8e
Radosław Jędryszczak <socjopata@gmail.com>
2019-08-06 08:38:31 +0200

    Forward requests in a smarter way actually

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'react_app#index'
-
   namespace :api do
     namespace :v1 do
       resources :heros, only: [:index]
     end
   end
-  get '*path' => redirect('/') #redirect everything to "home". This is basically for hard refresh of the current page
+
+  #redirect everything non-Ajax to "home". This is basically for hard refresh of the current page
+  get '*path', to: 'react_app#index', cosntraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+
+  root to: 'react_app#index'
 end
