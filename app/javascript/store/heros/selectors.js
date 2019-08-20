d6c4c85c245c80dcec9d66a73cd11b08148947c9
Radosław Jędryszczak <socjopata@gmail.com>
2019-08-20 16:21:10 +0200

    Use lodash for safely retreiving key from nested structure

--- app/javascript/store/heros/selectors.js
+++ app/javascript/store/heros/selectors.js
@@ -1,6 +1,8 @@
+import { get } from 'lodash';
+
 export const getHeroes = state => ({
   ...state.heroes.list,
   data: state.heroes.list.ids.map(id => state.heroes.byId[id]),
 });
 
-export const characterName = state => state.heros.character.name;
+export const characterName = state => get(state, "heros.character.name");
