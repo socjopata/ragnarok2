141c8f8fbc7b979b4bcdbae2e70a127cb0d135a4
Radosław Jędryszczak <socjopata@gmail.com>
2019-09-10 14:00:57 +0200

    Improve the formatting

--- app/javascript/store/heroes/selectors.js
+++ app/javascript/store/heroes/selectors.js
@@ -8,11 +8,11 @@ export const getHeroes = state => ({
 export const characterName = state => get(state, "heroes.character.name");
 export const characterId = state => get(state, "heroes.character.id");
 
-export const mainParameterBodyBuildingBase = state => {
+const _mainParameterBase = (state, name) => {
   if (state.heroes && state.heroes.byId) {
     const chosenHero = state.heroes.byId[parseInt(state.heroes.character.id)];
     if (chosenHero) {
-      const base = chosenHero.parameters.filter(parameter => parameter.name === 'physique')[0].value;
+      const base = chosenHero.parameters.filter(parameter => parameter.name === name)[0].value;
       return (base)
     } else {
       return 0
@@ -20,20 +20,10 @@ export const mainParameterBodyBuildingBase = state => {
   }
 };
 
+export const mainParameterBodyBuildingBase = (state) => _mainParameterBase(state, 'physique');
 export const mainParameterBodyBuildingFromImplants = state => 0;
 export const mainParameterBodyBuildingTotal = state => mainParameterBodyBuildingBase(state) + mainParameterBodyBuildingFromImplants(state);
 
-export const mainParameterDexterityBase = state => {
-  if (state.heroes && state.heroes.byId) {
-    const chosenHero = state.heroes.byId[parseInt(state.heroes.character.id)];
-    if (chosenHero) {
-      const base = chosenHero.parameters.filter(parameter => parameter.name === 'dexerity')[0].value;
-      return (base)
-    } else {
-      return 0
-    }
-  }
-};
-
+export const mainParameterDexterityBase = state => _mainParameterBase(state, 'dexerity');
 export const mainParameterDexterityFromImplants = state => 0;
 export const mainParameterDexterityTotal = state => mainParameterDexterityBase(state) + mainParameterDexterityFromImplants(state);
