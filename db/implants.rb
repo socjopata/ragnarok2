ea9f7e25bf646c6242c892293c7faad00d7a72cb
Radosław Jędryszczak <socjopata@gmail.com>
2019-11-22 11:23:21 +0100

    Implants seeds...

--- db/implants.rb
+++ db/implants.rb
@@ -262,7 +262,7 @@ implant = Implant.create(
   money_cost: 4000,
   construction: "Technika / Hekseri"
 )
-implant.primary_parameters.create(name: "physique", value: 1)
+implant.main_parameters.create(name: "physique", value: 1)
 
 implant = Implant.create(
   kind: "Midgard",
@@ -273,7 +273,7 @@ implant = Implant.create(
   money_cost: 4000,
   construction: "Technika / Hekseri"
 )
-implant.primary_parameters.create(name: "dexterity", value: 1)
+implant.main_parameters.create(name: "dexterity", value: 1)
 
 implant = Implant.create(
   kind: "Midgard",
@@ -284,7 +284,7 @@ implant = Implant.create(
   money_cost: 4000,
   construction: "Technika / Hekseri"
 )
-implant.primary_parameters.create(name: "perception", value: 1)
+implant.main_parameters.create(name: "perception", value: 1)
 
 implant = Implant.create(
   kind: "Midgard",
@@ -295,7 +295,7 @@ implant = Implant.create(
   money_cost: 4000,
   construction: "Technika / Hekseri"
 )
-implant.primary_parameters.create(name: "inteligence", value: 1)
+implant.main_parameters.create(name: "inteligence", value: 1)
 
 implant = Implant.create(
   kind: "Midgard",
@@ -306,7 +306,7 @@ implant = Implant.create(
   money_cost: 4000,
   construction: "Technika / Hekseri"
 )
-implant.primary_parameters.create(name: "self_control", value: 1)
+implant.main_parameters.create(name: "self_control", value: 1)
 
 implant = Implant.create(
   kind: "Midgard",
@@ -317,4 +317,287 @@ implant = Implant.create(
   money_cost: 4000,
   construction: "Technika / Hekseri"
 )
-implant.primary_parameters.create(name: "entropy", value: 1)
+implant.main_parameters.create(name: "entropy", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_improvement__entropy",
+  name: "Cybernetyczne Udoskonalenie (Entropia)",
+  description: "wybrany Parametr Główny Herosa zwiększa się o 1pkt. Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innego PG.",
+  neurostability_cost: 5,
+  money_cost: 4000,
+  construction: "Technika / Hekseri"
+)
+implant.main_parameters.create(name: "entropy", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__fencing",
+  name: "Cybernetyczna Perfekcja (Broń Biała)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "fencing", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__martial_arts",
+  name: "Cybernetyczna Perfekcja (Walka Wręcz)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "martial_arts", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__endurance",
+  name: "Cybernetyczna Perfekcja (Wytrzymałość)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "endurance", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__pistols",
+  name: "Cybernetyczna Perfekcja (Broń Krótka)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "pistols", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__vehicle_handling",
+  name: "Cybernetyczna Perfekcja (Obsługa Pojazdów)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "vehicle_handling", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__reflex",
+  name: "Cybernetyczna Perfekcja (Refleks)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "reflex", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__rifles",
+  name: "Cybernetyczna Perfekcja (Broń Długa)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "rifles", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__spaceships_handling",
+  name: "Cybernetyczna Perfekcja (Gwiezdny Pilotaż)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "spaceships_handling", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__senses",
+  name: "Cybernetyczna Perfekcja (Wyczulone Zmysły)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "senses", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__connections",
+  name: "Cybernetyczna Perfekcja (Koneksje)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "connections", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__technology",
+  name: "Cybernetyczna Perfekcja (Technika)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "technology", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__knowledge",
+  name: "Cybernetyczna Perfekcja (Wiedza)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "knowledge", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__hacking",
+  name: "Cybernetyczna Perfekcja (Hackowanie)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "hacking", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__infiltration",
+  name: "Cybernetyczna Perfekcja (Infiltracja)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "infiltration", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__trick_and_subterfuge",
+  name: "Cybernetyczna Perfekcja (Trik i Fortel)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "trick_and_subterfuge", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__charisma",
+  name: "Cybernetyczna Perfekcja (Charyzma)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "charisma", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__hexeri",
+  name: "Cybernetyczna Perfekcja (Hekseri)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "hexeri", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "cybernetic_perfection__willpower",
+  name: "Cybernetyczna Perfekcja (Siła Woli)",
+  description: "Wszczep zwiększa dowolną Umiejętność o +1pkt.  Wszczep może być zainstalowany wielokrotnie jednak za każdym razem musi dotyczyć innej Umiejętności.",
+  neurostability_cost: 3,
+  money_cost: 1500,
+  construction: "Technika / Hekseri"
+)
+implant.secondary_parameters.create(name: "willpower", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "gere_perception",
+  name: "Percepcja Gere",
+  description: "Heros zwiększa swoją Obronę w Zwarciu o +1pkt.",
+  neurostability_cost: 2,
+  money_cost: 500,
+  construction: "Technika / Hekseri"
+)
+implant.auxiliary_parameters.create(name: "close_combat_defense", bonus_type: "close_combat_defense", value: 1)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "freke_perception",
+  name: "Percepcja Freke",
+  description: "Heros zwiększa swoją Obronę Dystansową o +1pkt.",
+  neurostability_cost: 2,
+  money_cost: 500,
+  construction: "Technika / Hekseri"
+)
+implant.auxiliary_parameters.create(name: "ranged_defense", bonus_type: "ranged_defense", value: 1)
+
+Implant.create(
+  kind: "Midgard",
+  internal_name: "medic_pouch",
+  name: "Kieszeń Medyczna",
+  description: "Heros posiada specjalnie zaprojektowaną „kieszeń”, która może być ukryta jednak cena wzrasta do 100S, przestrzeń wewnątrz swego organizmu, w której może trzymać do pięciu „ampułek-ładunków” leków lub innych wybranych środków. Całość podłączona jest do organizmu postaci dzięki czemu może dowolnie aktywować ich aplikację.",
+  neurostability_cost: 1,
+  money_cost: 100,
+  construction: "Technika / Hekseri"
+)
+
+Implant.create(
+  kind: "Midgard",
+  internal_name: "cyberskin",
+  name: "Cyberskóra",
+  description: "postać zyskuje możliwość zmiany koloru skóry/karnacji. Wykonując testy Tricku i Fortelu związane ze zmianą tożsamości lub podszywaniem się pod kogoś Heros zyskuje +3pkt.",
+  neurostability_cost: 3,
+  money_cost: 700,
+  construction: "Technika / Hekseri"
+)
+
+implant = Implant.create(
+  kind: "Midgard",
+  internal_name: "only_a_scratch",
+  name: "To tylko Draśnięcie",
+  description: "raz na 12godzin postać może zredukować ilość otrzymanych w walce w zwarciu ran o połowę (po odliczeniu wartości ochronnej pancerza). Witalność Herosa zwiększana jest o +5pkt. ",
+  neurostability_cost: 3,
+  money_cost: 750,
+  construction: "Technika / Hekseri"
+)
+implant.virtual_parameters.create(name: "hit_points", value: 5)
+
+Implant.create(
+  kind: "Midgard",
+  internal_name: "1000_faces",
+  name: "1.000 Twarzy",
+  description: "postać zyskuje możliwość zmiany rysów twarzy, dzięki czemu może zwiększyć lub zmniejszyć swoją Prezencję o +/-5pkt. Wykonując testy Tricku i Fortelu związane ze zmianą tożsamości lub podszywaniem się pod kogoś Heros otrzymuje premię +3pkt.",
+  neurostability_cost: 3,
+  money_cost: 1250,
+  construction: "Technika / Hekseri"
+)
+
+Implant.create(
+  kind: "Midgard",
+  internal_name: "last_word",
+  name: "Ostatnie Słowo",
+  description: "na końcu rundy, w której umarł Heros, ładunek zespolony z wszczepem eksploduje zadając wszystkim istotom w zasięgu 5metrów 3k20obrażeń. Wszczep może zostać zainstalowany również w innym celu np.: kiedy nosząca go postać opuści określony teren dochodzi do wybuchu lub kiedy właściciel sam aktywuje jego moc, itp.",
+  neurostability_cost: 1,
+  money_cost: 500,
+  construction: "Technika / Hekseri"
+)
+
